9587d15e21f80dcfc2111d45baf7b94bd87f4431
edsonsimplicio <edson@hf-hidroforte.com.br>
2017-09-25 14:42:27 -0300

    Add files via upload

---
+++ Login.pas
@@ -0,0 +1,30 @@
+unit Login;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Imaging.jpeg, Vcl.ExtCtrls,
+  Vcl.Buttons, Vcl.StdCtrls;
+
+type
+  TForm2 = class(TForm)
+    IMG_Login: TImage;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    SpeedButton1: TSpeedButton;
+    SpeedButton2: TSpeedButton;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.dfm}
+
+end.
